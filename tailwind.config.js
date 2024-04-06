bca9b52dc7b8daa30ee8725098e0cd9390208fb0
ISMAILMENDOUN <mendoun.ismail@gmail.com>
2024-04-06 00:07:35 +0000

    ResponsivePlanetBank

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./dis/*.{html,js}"],
   theme: {
+    screens: {
+      'xs': '320px', // Example width for extra small screens
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+  
+    
     extend: {},
   },
   plugins: [],
